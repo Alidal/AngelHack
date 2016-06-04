1ad9787fcb50977da5be3bbcea2f88acc255a2ba
Vlad Sydorenko <vladyslav.sydorenko@gmail.com>
2016-06-04 15:45:29 +0300

    Temporary commit for gp5 convertation

---
+++ musichub/track/converter.py
@@ -0,0 +1,88 @@
+import os
+import guitarpro
+
+
+def generate_header(song):
+    song_text = [line.lyrics for line in song.lyrics.lines]
+
+    if any(song_text):
+        song_text = "\nW: ".join(song_text)
+    else:
+        song_text = ""
+
+    meta = "X: 1\n"
+    meta += "T: %s - %s\n" % (song.artist, song.title)  # Title
+    meta += "C: %s\n" % song.music  # Composer
+    key = song.key.name.replace("Major", "")
+    meta += "K: %s\n" % key  # Tonality
+    meta += "M: %s\n" % "C"  # TODO: Replace.
+    meta += "Q: %d\n" % song.tempo  # Tempo
+    meta += "W: %s\n" % song_text  # Song words
+    return meta
+
+
+def get_abc_note_from_midi(code):
+    table = {
+        0: 'C',
+        1: '^C',
+        2: 'D',
+        3: '^D',
+        4: 'E',
+        5: 'F',
+        6: '^F',
+        7: 'G',
+        8: '^G',
+        9: 'A',
+        10: '^A',
+        11: 'B',
+    }
+
+    note = table[code % 12]
+    octave = int((code - 60) / 12)
+    if octave < 0:
+        note += "".join(',' * (-octave))
+    elif octave > 0:
+        note += "".join('\'' * octave)
+    return note
+
+
+def duration(note):
+    from fractions import Fraction
+    duration = Fraction(str(note.durationPercent))
+    return str("%i/%i" % (duration.numerator, duration.denominator))
+
+
+def add_effects(note, effects):
+    if effects.staccato:
+        note = '.' + note
+    #if effects.acce
+    return note
+
+
+def convert(track):
+    file_path = os.path.join(track.repository.split('.')[0], track.title)
+    song = guitarpro.parse(file_path)
+
+    tracks = {}
+    # All tracks
+    for track in song.tracks:
+        headers = generate_header(track.song)
+        measures = [headers, ]
+        # Every measure
+        for measure in track.measures:
+            cur_measure = ""
+            # Every beat
+            for beat in measure.voices[0].beats:
+                cur_beat = "["
+                # Every note in chord (if it is chord)
+                for note in beat.notes:
+                    abc_note = get_abc_note_from_midi(note.realValue)
+                    abc_note += duration(note)
+                    abc_note = add_effects(abc_note, note.effect)
+                    cur_beat += abc_note
+                cur_beat += "] "
+                cur_measure += cur_beat
+            measures.append(cur_measure)
+        measures[-1] += " |]"
+        tracks[track.name] = "| ".join(measures)
+    return tracks
